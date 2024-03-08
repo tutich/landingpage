022a0ab3dc6d0f3d29e257cad992e1a02f61e811
tutich <pkoinange02@gmail.com>
2024-03-08 22:11:44 +0300

    body jsx

--- client/src/components/Body/Body.jsx
+++ client/src/components/Body/Body.jsx
@@ -1,5 +1,11 @@
+import './Body.css';
+
 import bgMobile from '../../images/image-mockups.png';
-import bgDesktop from '../../images/bg-intro-desktop.svg';
+import bgDesktop from '../../images/image-mockups.png';
+import online from '../../images/icon-online.svg';
+import budgeting from '../../images/icon-budgeting.svg';
+import Onboarding from '../../images/icon-onboarding.svg';
+import API from '../../images/icon-api.svg';
 
 function Body() {
     return (
@@ -10,7 +16,40 @@ function Body() {
               saving, budgeting, investing, and much more.
          </p>
          <div className="request"><button type='submit'>Request Invite</button></div>
+         <div className="desktop"><img src={bgDesktop} alt="bg" /></div>
+
+         <h2>Why choose Easybank?</h2>
+         <p>We leverage Open Banking to turn your bank account into your financial hub. 
+            Control your finances like never before.</p>
+
+            <img src={online} alt="" />
+            <h2>Online Banking </h2>
+            <p>Our modern web and mobile applications allow you to keep 
+                track of your finances wherever you are in the world.
+            </p>
+
+            <img src={budgeting} alt="" />
+            <h2>Simple Budgeting </h2>
+            <p>See exactly where your money goes each month. 
+                Receive notifications when you’re close to hitting your limits.
+            </p>
+
+            <img src={Onboarding} alt="" />
+            <h2>Fast Onboarding </h2>
+            <p>We don’t do branches. Open your account in minutes online and 
+                start taking control of your finances right away.
+            </p>
+
+            <img src={API} alt="" />
+            <h2>Open API</h2>
+            <p> Manage your savings, investments, pension, and much more from one account. 
+                Tracking your money has never been easier.
+            </p>
+            
+                
+                
         </>
+
     )
 }
 
